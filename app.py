8343bb2fad2348388e4c138efc10b15ec5f1ddd7
Nikolay (Mansas) <dokykin0nikolay@gmail.com>
2024-03-02 18:52:18 +0500

    Refactoring, add login request for add methods

--- app.py
+++ app.py
@@ -1,30 +1,19 @@
-from flask import Flask, render_template, request, redirect, session, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from base64 import b64encode
+
+from flask import Flask, jsonify, redirect, render_template, request, session
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///course.db'
-db = SQLAlchemy(app)
 app.secret_key = 'super_secret_key'
 
-
-class Teacher(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    teacher_name = db.Column(db.String(100), nullable=False)
-    subject = db.Column(db.String(100), nullable=False)
-    short_description = db.Column(db.String(200), nullable=False)
-    teacher_img = db.Column(db.Text, nullable=False)
+db = SQLAlchemy(app)
 
 
 class Admin(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
-
-
-class PhoneNumber(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    number = db.Column(db.String(20), unique=True, nullable=False)
 
 
 class Course(db.Model):
@@ -40,98 +29,88 @@ class Course(db.Model):
     img_course_data = db.Column(db.Text, nullable=False)
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+class PhoneNumber(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.String(20), unique=True, nullable=False)
+
+
+class Teacher(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    teacher_name = db.Column(db.String(100), nullable=False)
+    subject = db.Column(db.String(100), nullable=False)
+    short_description = db.Column(db.String(200), nullable=False)
+    teacher_img = db.Column(db.Text, nullable=False)
+
+
+@app.route('/')
+def index():
+    course = Course.query.order_by(Course.price).all()
+    teacher = Teacher.query.order_by(Teacher.teacher_name).all()
+
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        admin = Admin.query.filter_by(username=username, password=password).first()
-        if admin:
-            session['logged_in'] = True
-            return redirect('/admin')
-        else:
-            return render_template('login.html', error='Invalid credentials')
-    return render_template('login.html')
+        number = request.form['number']
 
+        existing_number = PhoneNumber.query.filter_by(number=number).first()
 
-@app.route('/delete_course/<int:course_id>', methods=['DELETE'])
-def delete_course(course_id):
-    if 'logged_in' not in session or not session['logged_in']:
-        return jsonify({'error': 'Unauthorized access'}), 401
+        if not existing_number:
+            new_number = PhoneNumber(number=number)
 
-    course = Course.query.get(course_id)
-    if not course:
-        return jsonify({'error': 'Course not found'}), 404
-
-    db.session.delete(course)
-    db.session.commit()
-    return jsonify({'message': 'Course deleted successfully'}), 200
-
-
-@app.route('/edit_course/<int:id>', methods=['GET', 'POST'])
-def edit_course(id):
-    course = Course.query.get(id)
-    if course:
-        if request.method == 'POST':
-            class_name = request.form['class_name']
-            course_name = request.form['course_name']
-            short_description = request.form['short_description']
-            price = request.form['price']
-            teacher_name = request.form['teacher_name']
-            teacher_img = request.files.get('teacher_img', None)
-            course_img = request.files.get('course_img', None)
-
-            course.class_name = class_name
-            course.course_name = course_name
-            course.short_description = short_description
-            course.price = price
-            course.img_teacher_name = teacher_name
-
-            if teacher_img:
-                course.img_teacher_data = b64encode(teacher_img.stream.read()).decode('utf-8')
-            if course_img:
-                course.img_course_data = b64encode(course_img.stream.read()).decode('utf-8')
-
+            db.session.add(new_number)
             db.session.commit()
 
-            return jsonify({'message': 'Course edited successfully'}), 200
-        else:
-
-            return render_template('edit_course.html', course=course)
-    else:
-
-        return jsonify({'error': 'Course not found'}), 404
+    return render_template('index.html', course=course, teacher=teacher)
 
 
 @app.route('/admin', methods=['GET'])
 def admin():
     if 'logged_in' not in session or not session['logged_in']:
         return redirect('/login')
+
+    phone_numbers = PhoneNumber.query.all()
+
+    return render_template('admin.html', phone_numbers=phone_numbers)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     if request.method == 'POST':
-        # Handle POST request if needed
-        pass
-    else:
-        phone_numbers = PhoneNumber.query.all()
-        return render_template('admin.html', phone_numbers=phone_numbers)
+        username = request.form['username']
+        password = request.form['password']
+
+        is_admin = Admin.query.filter_by(username=username, password=password).first()
+
+        if is_admin:
+            session['logged_in'] = True
+
+            return redirect('/admin')
+
+        return render_template('login.html', error='Invalid credentials')
+
+    return render_template('login.html')
 
 
 @app.route('/logout')
 def logout():
     session.clear()
+
     return redirect('/')
 
 
 @app.route('/create_course', methods=['POST', 'GET'])
 def create_course():
-    print(request.form)
+    if ('logged_in' not in session) or (not session['logged_in']):
+        return jsonify({'error': 'Unauthorized access'}), 401
+
     if request.method == 'POST':
         class_name = request.form['class_name']
         course_name = request.form['course_name']
         short_description = request.form['short_description']
         price = request.form['price']
         teacher_name = request.form['teacher_name']
+
         teacher_img = request.files['teacher_img']
         course_img = request.files['course_img']
+
         course = Course(class_name=class_name,
                         course_name=course_name,
                         img_course_data=b64encode(course_img.stream.read()).decode('utf-8'),
@@ -144,18 +123,21 @@ def create_course():
         db.session.commit()
 
         return redirect('/')
-    print(request.form)
 
     return render_template('create_course.html')
 
 
 @app.route('/create_teacher', methods=['POST', 'GET'])
 def create_teacher():
+    if ('logged_in' not in session) or (not session['logged_in']):
+        return jsonify({'error': 'Unauthorized access'}), 401
+
     if request.method == 'POST':
         teacher_name = request.form['teacher_name']
         subject = request.form['subject']
         short_description = request.form['short_description']
-        teacher_img = request.files['teacher_img']
+
+        teacher_img = request.files.get('teacher_img', None)
 
         teacher = Teacher(teacher_name=teacher_name,
                           subject=subject,
@@ -168,20 +150,6 @@ def create_teacher():
         return redirect('/')
 
     return render_template('create_teacher.html')
-
-
-@app.route('/')
-def index():
-    course = Course.query.order_by(Course.price).all()
-    teacher = Teacher.query.order_by(Teacher.teacher_name).all()
-    if request.method == 'POST':
-        number = request.form['number']
-        existing_number = PhoneNumber.query.filter_by(number=number).first()
-        if not existing_number:
-            new_number = PhoneNumber(number=number)
-            db.session.add(new_number)
-            db.session.commit()
-    return render_template('index.html', course=course, teacher=teacher)
 
 
 @app.route('/about', methods=['GET'])
@@ -197,6 +165,7 @@ def contact():
 @app.route('/courses', methods=['GET'])
 def courses():
     course = Course.query.order_by(Course.price).all()
+
     return render_template('courses.html', course=course)
 
 
@@ -208,11 +177,63 @@ def pricing():
 @app.route('/trainers', methods=['GET'])
 def trainers():
     teacher = Teacher.query.order_by(Teacher.teacher_name).all()
+
     return render_template('trainers.html', teacher=teacher)
 
 
+@app.route('/edit_course/<int:course_id>', methods=['GET', 'POST'])
+def edit_course(course_id):
+    course = Course.query.get(course_id)
+
+    if course:
+        if request.method == 'POST':
+            class_name = request.form['class_name']
+            course_name = request.form['course_name']
+            short_description = request.form['short_description']
+            price = request.form['price']
+            teacher_name = request.form['teacher_name']
+
+            teacher_img = request.files.get('teacher_img', None)
+            course_img = request.files.get('course_img', None)
+
+            course.class_name = class_name
+            course.course_name = course_name
+            course.short_description = short_description
+            course.price = price
+            course.img_teacher_name = teacher_name
+
+            if teacher_img:
+                course.img_teacher_data = b64encode(teacher_img.stream.read()).decode('utf-8')
+
+            if course_img:
+                course.img_course_data = b64encode(course_img.stream.read()).decode('utf-8')
+
+            db.session.commit()
+
+            return jsonify({'message': 'Course edited successfully'}), 200
+
+        return render_template('edit_course.html', course=course)
+
+    return jsonify({'error': 'Course not found'}), 404
+
+
+@app.route('/delete_course/<int:course_id>', methods=['DELETE'])
+def delete_course(course_id):
+    if ('logged_in' not in session) or (not session['logged_in']):
+        return jsonify({'error': 'Unauthorized access'}), 401
+
+    course = Course.query.get(course_id)
+
+    if not course:
+        return jsonify({'error': 'Course not found'}), 404
+
+    db.session.delete(course)
+    db.session.commit()
+
+    return jsonify({'message': 'Course deleted successfully'}), 200
+
+
 with app.app_context():
-    db.create_all()
     if not Admin.query.filter_by(username='admin').first():
         admin = Admin(username='admin', password='admin')
         db.session.add(admin)
